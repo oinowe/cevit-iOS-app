e383a8bd5695d0cc1ec1370b09836cc1358b04c5
Oscar <37719618+oinowe@users.noreply.github.com>
2024-02-19 17:04:54 -0800

    Initial Commit

---
+++ LabCevit/LabCevitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabCevitApp.swift
+//  LabCevit
+//
+//  Created by Oscar Inowe on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LabCevitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
