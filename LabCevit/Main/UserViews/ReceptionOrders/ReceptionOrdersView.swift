9ccc48c6919e6f4fae22081463a323983d0b55d2
Oscar <37719618+oinowe@users.noreply.github.com>
2024-04-24 14:35:49 -0700

    Change names and move classes to corresponding folders for organization

---
+++ LabCevit/Main/UserViews/ReceptionOrders/ReceptionOrdersView.swift
@@ -0,0 +1,18 @@
+//
+//  ReceptionOrdersView.swift
+//  LabCevit
+//
+//  Created by Oscar Inowe on 24/04/24.
+//
+
+import SwiftUI
+
+struct ReceptionOrdersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReceptionOrdersView()
+}
---
+++ LabCevit/Main/UserViews/ReceptionOrders/ReceptionOrdersViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ReceptionOrdersViewModel.swift
+//  LabCevit
+//
+//  Created by Oscar Inowe on 24/04/24.
+//
+
+import Foundation
+
+class ReceptionOrdersViewModel: ObservableObject {
+    init() {
+        
+    }
+}
